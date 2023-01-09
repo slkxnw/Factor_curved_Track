ac419b386e2cab48a6cb02603f8c1e66b5178054
slkxnw <cz1999615@163.com>
2023-01-09 22:16:03 +0800

    设计一个backend,接收来自匹配环节的匹配结果和接收航迹管理模块的航迹生成/合并/消亡指令,并对相应frontend进行处理

--- include/mybackend.h
+++ include/mybackend.h
@@ -16,11 +16,22 @@ public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
     typedef std::unique_ptr<myBackend> Ptr;
     typedef std::unordered_map<unsigned long, myFrontend::Ptr> Frontendtype;
-    typedef std::unordered_map<unsigned long, Vec3> Vec3list;
+    typedef std::unordered_map<unsigned long, Vec6> PredictObjtype;
 
-    void InitFrontend();
+    //和航迹管理有关
+    //生成新航迹
+    void InitObj(std::vector<Vec7> od_res);
+    //合并航迹
+    void ConcatObj(unsigned long old_obj_id, unsigned long new_obj_id);
+    //删除航迹
+    void StopObj(unsigned long id);
 
-    void UpdateObjState(unsigned long id, Vec7 od_reault);
+    //和数据匹配结果相关
+    //将检测结果添加到对应的frontend，并且更新
+    void UpdateObjState(std::unordered_map<unsigned long, Vec7> matches);
+
+    //给出，给定时刻的目标位置预测结果
+    PredictObjtype GetStatePrediction(double time);
 
 
 private:
@@ -28,8 +39,7 @@ private:
     unsigned long num_of_obj = 0;
 
     Frontendtype obj_list_;
-    Vec3list predict_postion_list_;
-    Vec3list obj_size_list_;
+    PredictObjtype state_prediction_list_;
 
 
 };

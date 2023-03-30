62b901484e3d8ff04cc4d388af92e925024426da
slkxnw <cz1999615@163.com>
2023-03-30 20:44:49 +0800

    结果有错误,跟踪结果和之前不一致

--- scripts/trk_predict_posi.py
+++ scripts/trk_predict_posi.py
@@ -15,7 +15,7 @@ def parse_args():
     parser.add_argument('--datadir', type=str, default='/home/chenz/GD/dataset')
     parser.add_argument('--dataset', type=str, default='KITTI', help='KITTI, nuScenes')
     parser.add_argument('--split', type=str, default='training', help='training, testing')
-    parser.add_argument('--seqs', type=str, default='0019')
+    parser.add_argument('--seqs', type=str, default='0012')
     parser.add_argument('__name', type=str)
     parser.add_argument('__log', type=str)
     args = parser.parse_args()

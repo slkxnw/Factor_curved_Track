8817f8db81ddc6fb70ab5ef419125e140a72433d
slkxnw <cz1999615@163.com>
2023-12-28 21:43:22 +0800

    add CTRA-EKF, not finish, could not be used

--- include/factor_curved_track/myEKF.h
+++ include/factor_curved_track/myEKF.h
@@ -85,6 +85,53 @@ private:
 };
 
 
+class CTRA_EKF
+{
+
+public:
+
+    CTRA_EKF(Vec6 init_state);
+
+    void calJacobi(double dt);
+    
+    void predict(double dt);
+
+    void update(Vec3 measure);
+
+    Vec6 GetState()
+    {
+        return x_state;
+    }
+    Vec6 GetStatePred()
+    {
+        return x_state_pred;
+    }
+
+private:
+
+    //状态转移方程/运动方程变换矩阵参数
+    //x_t+1 = A * x_t + B * u + w//这里没有控制量
+    //w是一个多元高斯噪声，均值为0， 方差为Q
+    Mat66 J;
+    Mat66 Q;
+    //观测方程
+    //z_t = C * x_t + v
+    //v是一个多元高斯噪声，均值为0，方差为R
+    Mat36 C;
+    Mat33 R = Mat33::Identity();
+    //状态是一个高斯分布，均值为x_state，方差为P
+    Vec6 x_state;
+    Vec6 x_state_pred;
+    Mat66 P;
+    Mat66 P_pred;
+
+    double delta_ax,delta_ay, delta_ath;
+
+
+
+};
+
+
 } // namespace mytrk
 
 
